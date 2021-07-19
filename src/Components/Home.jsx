47f17daa7f4592b7045ff6f0df901dda0842fdf6
Andrea Catania <andreacatania91@gmail.com>
2021-07-19 14:11:35 +0100

    add Find out more in home and Router in Index.js

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="columns">
       <div className="column is-7">
         <p className="title">Hi, I'm Andrea.</p>
         <p className="subtitle mt-4">Junior Full Stack Developer</p>
-        <p className="is-size-5 mt-3"></p>
+        <p className="is-size-6 mt-3">Trained at Nortchoders</p>
+        <Link to="/About">
+          <p className="has-text-weight-medium is-waring mt-3 has-text-danger">
+            Find out more {">"}
+          </p>
+        </Link>
       </div>
     </div>
   );
