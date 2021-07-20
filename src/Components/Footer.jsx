b9b795ae34b45124c6609465243e65e01f6299be
Andrea Catania <andreacatania91@gmail.com>
2021-07-20 14:08:31 +0100

    enable links in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,23 +3,40 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="hero-foot">
       <div className="mb-3">
-        <span className="icon mx-3">
-          <FontAwesomeIcon icon={faGithub} />
-        </span>
-        <span className="icon mx-3">
-          <FontAwesomeIcon icon={faLinkedin} />
-        </span>
-        <span className="icon mx-3">
-          <FontAwesomeIcon icon={faTwitter} />
-        </span>
-        <span className="icon mx-3">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </span>
+        <a href="https://github.com/Angaca" target="_blank" rel="noreferrer">
+          <span className="icon mx-3">
+            <FontAwesomeIcon icon={faGithub} />
+          </span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/andreacatania/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="icon mx-3">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </span>
+        </a>
+        <a
+          href="https://twitter.com/Angaca_VII"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="icon mx-3">
+            <FontAwesomeIcon icon={faTwitter} />
+          </span>
+        </a>
+        <Link to="/Contact">
+          <span className="icon mx-3">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+        </Link>
       </div>
     </div>
   );
