a9fce41e3fe570e607f5340363582a4070662084
Andrea Catania <andreacatania91@gmail.com>
2021-07-23 09:42:02 +0100

    fix a typo in the homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="ml-3">
       <p className="title is-size-1-tablet is-spaced">Hi, I'm Andrea.</p>
       <p className="subtitle is-size-3-tablet">Junior Full Stack Developer</p>
-      <p className="is-size-6 mt-3">Trained at Nortchoders</p>
+      <p className="is-size-6 mt-3">Trained at Northcoders</p>
       <Link to="/About">
         <p className="has-text-weight-medium is-waring mt-3 has-text-danger">
           Find out more {">"}
